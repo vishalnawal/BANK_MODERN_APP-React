dc323e1eade6b7f48a77882a84baa3674c27ef5c
vishalnawal <vishalmaheshwari2501@gmail.com>
2023-07-03 08:00:57 +0530

    Updated Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -35,13 +35,10 @@ const Hero = () => {
       </div>
 
       <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
-        <img src={robot} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
-
-        {/* gradient start */}
+        <img src={robot} alt="billing" className="w-[100%] h-[100%] relative z-index[5]"/>
         <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
-        <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
+        <div className="absolute z-[0] w-[80%] h-[80%] rounded-full bottom-40 white__gradient" />
         <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
-        {/* gradient end */}
       </div>
 
       <div className={`ss:hidden ${styles.flexCenter}`}>
